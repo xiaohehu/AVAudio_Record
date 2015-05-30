0737606d63c85bb283cdc5d7a82efb0738487372
Xiaohe <huxiaohe113@gmail.com>
2015-05-29 21:15:02 -0400

    Added pitch function buttons

--- Pinch Perfect/PlaySoundsViewController.swift
+++ Pinch Perfect/PlaySoundsViewController.swift
@@ -13,6 +13,8 @@ class PlaySoundsViewController: UIViewController {
 
     var audioPlayer : AVAudioPlayer!
     var receivedAudio : RecordAudio!
+    var audioEngine : AVAudioEngine!
+    var audioFile : AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,9 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,6 +50,32 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.rate = speed
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
+    }
+    @IBAction func playChipmunkAudio(sender: AnyObject) {
+            playAudioWithVariblePitch(1000)
+    }
+    @IBAction func playDarthvaderAudio(sender: AnyObject) {
+            playAudioWithVariblePitch(-1000)
+    }
+    func playAudioWithVariblePitch(pitch: Float)
+    {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        audioPlayerNode.play()
     }
     @IBAction func stopAudio(sender: AnyObject) {
         audioPlayer.stop()
